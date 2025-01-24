ea82ce18fbe840a89002bbe2bcf0f759e2fcecc1
ayumu203 <ochiohita@gmail.com>
2025-01-24 21:18:56 +0900

    [update] ユーザがマッチング開始時にUserテーブルにユーザ情報が登録されるよう実装 マッチングシステムのルーム検索及びルームへの参加までを実装

---
+++ src/ConstValue.js
@@ -0,0 +1,1 @@
+export const MAX_ROOM_HEADCOUNT = 3;
--- src/Matching.jsx
+++ src/Matching.jsx
@@ -3,6 +3,7 @@ import { PageContext, UserContext } from "./App";
 import { Box, Button, Typography } from "@mui/material";
 import { collection, deleteDoc, getDocs,doc, updateDoc, onSnapshot, setDoc } from "firebase/firestore";
 import { db } from "./firebase";
+import { MAX_ROOM_HEADCOUNT } from "./ConstValue";
 
 function Matching(){
     const {user} = useContext(UserContext);
@@ -16,79 +17,135 @@ function Matching(){
     });
 
     const handleRoomMatching = async () => {
-        setIsMatching(true);
-        // await deleteRoom();
-        // const isJoined = await searchRoom();
-        await makeRoom();
+        storeUserData();
+        const joinableRoom = await searchRoom();
+        joinRoom(joinableRoom);
+        // rewriteFirestoreData();
     }
     
-    // roomに参加できた場合にはfirebase上のroomのデータに自身のデータを書き込んでtrueを返す,できなかった場合にfalseを返す
-    const searchRoom = async () => {
+    const storeUserData = async () =>{
+        await setDoc(doc(db,"User",user.uid),{
+            "UserName" : user.displayName,
+            "PhotoURL" : user.photoURL 
+        })
     }
-    const makeRoom = async () =>{
-        console.log("ルーム作成開始");
-        try {
-            console.log(null);
-        } catch(e){
-            console.log("error:",e);
+
+    // 参加可能なルームの番号を返す
+    const searchRoom = async () => {
+        console.log("空いているルームを検索");
+        const joinableRoom = [];
+        // 空いているルームを順に検索していく
+        const querySnapshot = await getDocs(collection(db, "MatchingRoom"));
+        querySnapshot.forEach(async (room) => {
+            const roomData = room.data();
+            if(roomData.HeadCount < MAX_ROOM_HEADCOUNT){
+                // ルームが空いていた場合は,そのルームに自身のUserIdを書き込み,ルームの人数が上限になるまで待機する.
+                console.log(`Room${roomData.RoomNumber}は空きがあります.`);
+                switch(roomData.HeadCount){
+                    case 0:
+                        joinableRoom.push(["Host",roomData.RoomNumber]);
+                        break;
+                    case 1:
+                        joinableRoom.push(["Sub1",roomData.RoomNumber]);
+                        break;
+                    case 2:
+                        joinableRoom.push(["Sub2",roomData.RoomNumber]);
+                        break;
+                    default:
+                        break ;
+                }
+            }
+        });  
+        return joinableRoom;
+    }
+
+    const joinRoom = (joinableRoom) =>{
+        for(let i = 0; i < joinableRoom.length; i++){
+            const roomRef = doc(db, 'MatchingRoom', `Room${joinableRoom[i][1]}`);
+            switch(joinableRoom[i][0]){
+                case "Host":
+                    updateDoc(roomRef, { "HostUserId" : user.uid });
+                    return ;
+                case "Sub1":
+                    updateDoc(roomRef, { "SubUser1Id" : user.uid });
+                    return ;
+                case "Sub2":
+                    updateDoc(roomRef, { "SubUser2Id" : user.uid });
+                    return;
+            }
         }
     }
 
-
+    // 複数個のドキュメントを一括で変更したい場合に使用する:やらかしたときのリカバー
+    const rewriteFirestoreData = () =>{
+        for(let i=1; i<= 5; i++){
+            const roomRef = doc(db, 'MatchingRoom', `Room${i}`);
+            setDoc(roomRef, {
+                "RoomNumber":i,
+                "HeadCount":0,
+                "HostUserId":"",
+                "SubUser1Id":"",
+                "SubUser2Id":""
+            });
+        }
+    }
 
     return (
-        <Box 
-            mt={"50px"}
-            sx={{
-                display:"flex",
-                justifyContent:"center",
-            }}
-        >  
-            {!user 
-                ? 
-                <Typography
-                    sx={{
-                        color:"whitesmoke",
-                        bgcolor:"silver",
-                        width:"100%",
-                        textAlign:"center"
-                    }}
-                >
-                    まずは右上のボタンよりログインしてください.
-                </Typography> 
-                :isMatching 
-                    ? 
-                        !isMatched 
-                        ? 
-                            <Typography
-                                sx={{
-                                    color:"whitesmoke",
-                                    bgcolor:"silver",
-                                    width:"100%",
-                                    textAlign:"center"
-                                }}        
-                            >現在マッチング中です.しばらくお待ち下さい...</Typography> 
-                        :
-                            <Typography
-                                sx={{
-                                    color:"whitesmoke",
-                                    bgcolor:"silver",
-                                    width:"100%",
-                                    textAlign:"center"
-                                }}        
-                            >マッチング完了</Typography>
-                    :
-                    <Button
-                        variant="contained"
-                        sx={{
-                            color:"white",
-                        }}
-                        onClick={()=>handleRoomMatching()}
-                    >
-                        マッチング開始
-                    </Button>
-            }
-        </Box>
+    <>
+        {user ? <Typography><Button onClick={handleRoomMatching}>マッチング開始</Button></Typography> : <Typography>まずは右上のボタンよりログインしてください</Typography>}
+    </>
+        // <Box 
+        //     mt={"50px"}
+        //     sx={{
+        //         display:"flex",
+        //         justifyContent:"center",
+        //     }}
+        // >  
+        //     {!user 
+        //         ? 
+        //         <Typography
+        //             sx={{
+        //                 color:"whitesmoke",
+        //                 bgcolor:"silver",
+        //                 width:"100%",
+        //                 textAlign:"center"
+        //             }}
+        //         >
+        //             まずは右上のボタンよりログインしてください.
+        //         </Typography> 
+        //         :isMatching 
+        //             ? 
+        //                 !isMatched 
+        //                 ? 
+        //                     <Typography
+        //                         sx={{
+        //                             color:"whitesmoke",
+        //                             bgcolor:"silver",
+        //                             width:"100%",
+        //                             textAlign:"center"
+        //                         }}        
+        //                     >現在マッチング中です.しばらくお待ち下さい...</Typography> 
+        //                 :
+        //                     <Typography
+        //                         sx={{
+        //                             color:"whitesmoke",
+        //                             bgcolor:"silver",
+        //                             width:"100%",
+        //                             textAlign:"center"
+        //                         }}        
+        //                     >マッチング完了</Typography>
+        //             :
+        //             <Button
+        //                 variant="contained"
+        //                 sx={{
+        //                     color:"white",
+        //                 }}
+        //                 onClick={()=>handleRoomMatching()}
+        //             >
+        //                 マッチング開始
+        //             </Button>
+        //     }
+        // </Box>
     )
 };
 
