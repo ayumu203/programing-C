9d86426a5e3d6ecb4ee818d5172ef5c1671ba7ed
ayumu203 <ochiohita@gmail.com>
2025-01-24 23:08:12 +0900

    [update]微細な変更

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,9 +1,15 @@
 import { useContext } from "react"
 import { UserContext } from "./App"
 import { Box, Typography } from "@mui/material";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "./firebase";
 
 export const Game = () =>{
     const {user} = useContext(UserContext);
+
+    const opponentUser1 = onSnapshot(doc(db,"MatchingRoom","HostUserId"),(doc) =>{
+        console.log(doc.data);
+    })
     // 人数が揃ったらゲームを開始する.
     // ターンごとに全員が回答を待機,そろったら表示
     return (
