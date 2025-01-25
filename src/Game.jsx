f250814310422f676e82f6a1a94b9faeb567efb0
ayumu203 <ochiohita@gmail.com>
2025-01-25 21:34:13 +0900

    [add]ランダムなゲームで使用する文字列を生成するファイルを作成

--- src/Game.jsx
+++ src/Game.jsx
@@ -4,6 +4,7 @@ import { Box, Typography } from "@mui/material";
 import { doc, getDoc, onSnapshot } from "firebase/firestore";
 import { db } from "./firebase";
 import { PlayerInfoPanel } from "./GameComponents/PlayerInfoPanel";
+import makeRandomRegText from "./Gametools/makeText";
 
 export const Game = () =>{
     const {user} = useContext(UserContext);
@@ -11,6 +12,7 @@ export const Game = () =>{
     const [ opponent1,setOpponent1 ] = useState(null);
     const [ opponent2,setOpponent2 ] = useState(null);
 
+    // 各プレイヤーの情報を管理する部分
     const userRef = doc(db,"MatchingRoom",`Room${roomNumber}`);
     const userDataObserver = onSnapshot(userRef,async (document) =>{
         if(user && document.exists()){
@@ -46,15 +48,15 @@ export const Game = () =>{
     return(
         <Box>
             <Box>
-                プレイヤーの情報を表示するコンポーネント
+                {/* プレイヤーの情報を表示するBox */}
+                {/* コンポーネント化+UIなんとかしてほしい */}
                 <PlayerInfoPanel></PlayerInfoPanel>
                 {/* {!opponent1 ? <CompetitorInfoPanel></CompetitorInfoPanel> : <CompetitorInfoPanel opponent={opponent1}></CompetitorInfoPanel>} */}
                 <Typography>{!opponent1 ? <>マッチング中</> : <>{opponent1.UserName}</>}</Typography>
                 {!opponent1 ? <>no img</>: <img src={`${opponent1.PhotoURL}`}></img>}        
-                {/* 写真も同じように作ってクレメンス、、、 */}
                 <Typography>{!opponent2 ? <>マッチング中</> : <>{opponent2.UserName}</>}</Typography>
-                {/* 写真も同じように作ってクレメンス、、、 */}
             </Box>
+                {makeRandomRegText()}
             <Box>
 
             </Box>
