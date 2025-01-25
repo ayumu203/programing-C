31d9943d1da20febcaad72d4bd6d6332198f2716
ayumu203 <ochiohita@gmail.com>
2025-01-26 01:30:07 +0900

    [fix]マッチングの際ホストではうまく対戦相手のデータを取得できない問題を修正.

--- src/ConstValue.js
+++ src/ConstValue.js
@@ -1,1 +1,2 @@
 export const MAX_ROOM_HEADCOUNT = 3;
+export const GAME_TURN = 3;
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,37 +1,59 @@
 import { useContext, useState } from "react"
 import { RoomContext, UserContext } from "./App"
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { doc, getDoc, onSnapshot } from "firebase/firestore";
 import { db } from "./firebase";
 import { PlayerInfoPanel } from "./GameComponents/PlayerInfoPanel";
 import makeRandomRegText from "./Gametools/makeText";
 
 export const Game = () =>{
-    const {user} = useContext(UserContext);
-    const {roomNumber} = useContext(RoomContext);
+    const { user } = useContext(UserContext);
+    const { roomNumber } = useContext(RoomContext);
     const [ opponent1,setOpponent1 ] = useState(null);
     const [ opponent2,setOpponent2 ] = useState(null);
-
+    
     // 各プレイヤーの情報を管理する部分
     const userRef = doc(db,"MatchingRoom",`Room${roomNumber}`);
     const userDataObserver = onSnapshot(userRef,async (document) =>{
         if(user && document.exists()){
+            if(user.uid === document.data().HostUserId){
+                setIsHost(true);
+                if(user.uid !== document.data().SubUser1Id){
+                    const oppRef = doc(db,"User",`${document.data().SubUser1Id}`);
+                    const opp = await getDoc(oppRef);
+                    if(opp.exists()){
+                        setOpponent1(opp.data());
+                    }
+                }
+                if(user.uid !== document.data().SubUser2Id){
+                    const oppRef = doc(db,"User",`${document.data().SubUser2Id}`);
+                    const opp = await getDoc(oppRef);
+                    if(opp.exists()){
+                        setOpponent2(opp.data());
+                    }    
+                }
+    
+            }
+            
             if(user.uid !== document.data().HostUserId){
                 const oppRef = doc(db,"User",`${document.data().HostUserId}`);
                 const opp = await  getDoc(oppRef);
                 if(opp.exists()){
-                    if(opponent1 === null)setOpponent1(opp.data());
+                    if(opponent1 === null){
+                        setOpponent1(opp.data());
+                    }
                 }
             }
             if(user.uid !== document.data().SubUser1Id){
                 const oppRef = doc(db,"User",`${document.data().SubUser1Id}`);
                 const opp = await getDoc(oppRef);
                 if(opp.exists()){
-                    if(opponent1 === null){
-                        setOpponent1(opp.data());
-                    }
-                    if(opponent1.UserName !== opp.data().UserName && opponent2 === null){
+                    if(opponent1 !== null && opponent1.UserName !== opp.data().UserName && opponent2 === null){
                         setOpponent2(opp.data());
+                    }
+                    else if(opponent1 === null){
+                        console.log(opponent1.UserName,":",opp.data().UserName);
+                        setOpponent1(opp.data());
                     }
                 }
             }
@@ -39,11 +61,27 @@ export const Game = () =>{
                 const oppRef = doc(db,"User",`${document.data().SubUser2Id}`);
                 const opp = await getDoc(oppRef);
                 if(opp.exists()){
-                    if(opponent2 === null)setOpponent2(opp.data());
+                    if(opponent2 === null){
+                        setOpponent2(opp.data());
+                    }
                 }    
             }
         }
     })  
+
+    // ゲームの処理を行う部分
+    const [ isHost,setIsHost ] = useState(false);
+    const [ turn,setTurn ] = useState(false);
+    const [ isStart,setIsStart ] = useState(false);
+    const roomRef = doc(db,"MatchingRoom",`Room${roomNumber}`);
+    // ルームの人数が規定人数になった時ゲームを開始する
+    // const roomHeadCountObserver = onSnapshot(roomRef,async (document) =>{
+    //     if(document.data().)
+    // });
+
+    // ホストの場合
+    // サブの場合
+    
 
     return(
         <Box>
@@ -55,10 +93,10 @@ export const Game = () =>{
                 <Typography>{!opponent1 ? <>マッチング中</> : <>{opponent1.UserName}</>}</Typography>
                 {!opponent1 ? <>no img</>: <img src={`${opponent1.PhotoURL}`}></img>}        
                 <Typography>{!opponent2 ? <>マッチング中</> : <>{opponent2.UserName}</>}</Typography>
+                {!opponent2 ? <>no img</>: <img src={`${opponent2.PhotoURL}`}></img>}        
             </Box>
-                {makeRandomRegText()}
             <Box>
-
+                {/* ちょっとコンポーネント化に手間がかかるのでベタ書きする */}
             </Box>
         </Box>
     )
