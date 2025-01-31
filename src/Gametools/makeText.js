d186fef18568ac9ec16cf91f7922b6ff119c1177
ayumu203 <ochiohita@gmail.com>
2025-01-31 18:27:34 +0900

    [update]文字列生成部分の乱数固定を解除,完全にランダムな数値が出るよう編集

--- src/Gametools/makeText.js
+++ src/Gametools/makeText.js
@@ -1,7 +1,7 @@
 export function makeRandomRegText(){
     let reg = null;
-    //const rand = Math.floor(Math.random() * 3);
-    const rand = 0;
+    const rand = Math.floor(Math.random() * 3);
+    // const rand = 0;
     switch(rand){
         case 0:
             reg = "^a.{n}a$";
